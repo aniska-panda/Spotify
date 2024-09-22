975b69a24e06d2c703ac5cc44d059b0735364970
aniska-panda <aniskapanda322@gmail.com>
2024-09-22 20:10:33 +0530

    albums

--- website/src/components/DisplayHome.jsx
+++ website/src/components/DisplayHome.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { albumsData } from '../assets/assets'
+import AlbumItem from './AlbumItem'
 
 const DisplayHome = () => {
   return (
     <>
-     <Navbar/> 
+     <Navbar/>
+     <div className='mb-4'>
+      <h1 className='my-5 font-bold text-2xl'>Featured charts</h1>
+      <div className='flex overflow-auto'>
+      {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+      </div>
+          
+      </div> 
     </>
   )
 }
--- website/src/components/Navbar.jsx
+++ website/src/components/Navbar.jsx
@@ -9,7 +9,16 @@ const Navbar = () => {
             <img className='w-8 bg-green-100 p-2 rounded-2xl cursor-pointer' src={assets.arrow_left} alt=""/>
             <img className='w-8 bg-green-100 p-2 rounded-2xl cursor-pointer' src={assets.arrow_right} alt=""/>
         </div>
-
+        <div className='flex items-center gap-4'>
+            <p className='bg-black text-white text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer'>Explore Premium</p>
+            <p className='bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer'>Insall App</p>
+            <p className='bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center'>A</p>
+        </div>
+     </div>
+     <div className='flex items-center gap-2 mt-4'>
+           <p className='bg-black text-white px-4 py-1 rounded-2xl cursor-pointer'>All</p>
+           <p className='bg-black px-4 py-1 rounded-2xl cursor-pointer'>Music</p>
+           <p className='bg-black px-4 py-1 rounded-2xl cursor-pointer'>Podcasts</p>
      </div>
     </>
   )
